d5eb0736290d37be39692ea817e68678f122d7ea
arman5625 <108667833+arman5625@users.noreply.github.com>
2023-08-20 04:37:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Developer","Freelancer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+console.log("i am running");
